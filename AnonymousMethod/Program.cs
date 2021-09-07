9c8d0039cebc80cbb0877b9ad7aba032bb42686f
Rushikesh Bhattad <rushikeshbhattad8@gmail.com>
2021-09-07 16:05:04 +0530

    Add project files.

---
+++ AnonymousMethod/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static AnonyomusMethodDemo.Anonyomus;
+
+namespace AnonyomusMethodDemo
+{
+    public delegate void DelegateSquare(int n);
+    class Anonyomus
+    {
+
+        internal void Anon(int x)
+        {
+            DelegateSquare delAnon = delegate (int a)
+            {
+                Console.Write(x * x);
+            };
+            delAnon(x);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Anonyomus anonyomus = new Anonyomus();
+            anonyomus.Anon(5);
+            Console.ReadKey();
+        }
+    }
+}
